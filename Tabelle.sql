085dde51ea0d5b6bc0763299c8ad0dbc72aae52b
nick <nicolobartolinii@gmail.com>
2022-12-23 18:02:56 +0100

    creazione tabelle

--- Tabelle.sql
+++ Tabelle.sql
@@ -51,31 +51,54 @@ Numero INTEGER,
 Data DATE,
 Importo INTEGER,
 Codice INTEGER,
-Quantità INTEGER)
-CREATE TABLE Richiesta(Cliente, Ordine)
-CREATE TABLE Indirizzo(
-Via VARCHAR(15),
-Civico INTEGER,
-Comune VARCHAR(2));
-CREATE TABLE Fornitore(
-Codice INTEGER,
-RagioneSociale VARCHAR(20),
-PartitaIVA VARCHAR(11));
-CREATE TABLE RubricaF(Fornitore, Contatto)
-CREATE TABLE Contatto(Recapito)
-CREATE TABLE Cliente(Codice, RagioneSociale, PartitaIVA)
-CREATE TABLE RubricaC(Contatto, Cliente)
-CREATE TABLE Stipulazione(Cliente, Documento)
-CREATE TABLE Documento(
-    Numero INTEGER,
-    DatiPagamento VARCHAR(30),
-    Data DATE,
-    Importo INTEGER,
-    TipoDocumento VARCHAR(10)
-CREATE TABLE MacchinaInLavorazione
-    (matricola INTEGER PRIMARY KEY ,
-    Codice VARCHAR(8));
-
-CREATE TABLE Composizione(Matricola INTEGER PRIMARY KEY ,
-Codice VARCHAR(8),
 Quantità INTEGER);
+
+CREATE TABLE Richiesta(
+    Cliente INTEGER NOT NULL REFERENCES Cliente(Codice),
+    Ordine INTEGER NOT NULL REFERENCES Ordine(Numero));
+
+CREATE TABLE Indirizzo(
+    Via VARCHAR(25) NOT NULL,
+    Civico INTEGER NOT NULL,
+    Comune VARCHAR(25) NOT NULL);
+
+CREATE TABLE Fornitore(
+    Codice INTEGER PRIMARY KEY AUTO_INCREMENT,
+    RagioneSociale VARCHAR(30) NOT NULL,
+    PartitaIVA VARCHAR(11) NOT NULL);
+
+CREATE TABLE RubricaF(
+    Fornitore INTEGER NOT NULL REFERENCES Cliente(Codice),
+    Contatto VARCHAR(30) NOT NULL REFERENCES Contatto(Recapito));
+
+CREATE TABLE Contatto(
+    Recapito VARCHAR(30) PRIMARY KEY);
+
+CREATE TABLE Cliente(
+    Codice INTEGER PRIMARY KEY AUTO_INCREMENT,
+    RagioneSociale VARCHAR(30) NOT NULL,
+    PartitaIVA VARCHAR(11) NOT NULL);
+
+CREATE TABLE RubricaC(
+    Cliente INTEGER NOT NULL REFERENCES Cliente(Codice),
+    Contatto VARCHAR(30) NOT NULL REFERENCES Contatto(Recapito));
+
+CREATE TABLE Stipulazione(
+    Cliente INTEGER NOT NULL REFERENCES Cliente(Codice),
+    Documento INTEGER NOT NULL REFERENCES Documento(Numero));
+
+CREATE TABLE Documento(
+    Numero INTEGER PRIMARY KEY AUTO_INCREMENT,
+    DatiPagamento VARCHAR(30) NOT NULL,
+    Data DATE NOT NULL,
+    Importo INTEGER NOT NULL,
+    TipoDocumento VARCHAR(10) NOT NULL);
+
+CREATE TABLE MacchinaInLavorazione(
+    Matricola INTEGER PRIMARY KEY ,
+    Codice VARCHAR(8) NOT NULL);
+
+CREATE TABLE Composizione(
+    Matricola INTEGER PRIMARY KEY,
+    Codice VARCHAR(8) NOT NULL,
+    Quantità INTEGER NOT NULL);
