912fcdcbb9c89ff3bf389650a3830c86ba2773e3
ivanpacenti <s1103326@studenti.univpm.it>
2022-12-24 16:15:40 +0100

    inserite chiavi tabelle

--- Tabelle.sql
+++ Tabelle.sql
@@ -1,62 +1,70 @@
 CREATE TABLE Locazione(
-CodiceScaffale VARCHAR (2)PRIMARY KEY ,
-Sezione_Magazzino VARCHAR(2)NOT NULL ,
-Scaffalatura VARCHAR(2) NOT NULL ,
-RigaScaff VARCHAR(2) NOT NULL ,
-ColonnaScaff VARCHAR (2) NOT NULL ,
-Codice_scaffale VARCHAR(2) NOT NULL );
+CodiceScaffale VARCHAR(8) PRIMARY KEY ,
+PesoOccupato INTEGER,
+Sezione CHAR(1),
+Scaffalatura CHAR(1),
+RigaScaff INTEGER,
+ColonnaScaff INTEGER);
 
 
 CREATE TABLE Ubicazione(
-Scaffale VARCHAR(2)NOT NULL ,
-Articolo VARCHAR(8) NOT NULL REFERENCES Articolo(Codice),
-Quantita INTEGER NOT NULL );
+    Locazione VARCHAR(2) REFERENCES Locazione(CodiceScaffale),
+    Articolo VARCHAR(8)  REFERENCES Articolo(Codice),
+    Quantita INTEGER NOT NULL );
 
 CREATE TABLE Articolo(
-Codice VARCHAR(8) PRIMARY KEY ,
-Descrizione VARCHAR(50) NOT NULL ,
-Quantità INTEGER NOT NULL ,
-Prezzo INTEGER ,
-Altezza INTEGER NOT NULL ,
-Larghezza INTEGER NOT NULL ,
-Profondità INTEGER NOT NULL ,
-Massa INTEGER NOT NULL );
+    Codice VARCHAR(8) PRIMARY KEY ,
+    Descrizione VARCHAR(50) NOT NULL ,
+    Quantità INTEGER NOT NULL ,
+    Prezzo INTEGER ,
+    Altezza INTEGER NOT NULL ,
+    Larghezza INTEGER NOT NULL ,
+    Profondità INTEGER NOT NULL ,
+    Massa INTEGER NOT NULL );
 
 CREATE TABLE DettaglioDistinta(
-Distinta VARCHAR(8) REFERENCES Distinta(Articolo_di_riferimento),
-Quantità INTEGER NOT NULL ,
-Codice VARCHAR (8));
+    Distinta VARCHAR(8) REFERENCES Distinta(ArticoloDiRiferimento),
+    Quantità INTEGER NOT NULL ,
+    Articolo VARCHAR (8) REFERENCES Articolo(Codice));
 
 CREATE TABLE Distinta(
-NumeroArticoli integer,
-CodiceDistinta VARCHAR(8));
+    NumeroArticoli INTEGER NOT NULL ,
+    ArticoloDiRiferimento VARCHAR(8));
+
 CREATE TABLE DettaglioCicli(
-Distinta VARCHAR(8),
-Ciclo VARCHAR(20));
-CREATE TABLE Ciclo(
-Tipologia VARCHAR(20));
+    Distinta VARCHAR(8) NOT NULL REFERENCES Distinta(ArticoloDiRiferimento),
+    Ciclo VARCHAR(20) NOT NULL REFERENCES CicloDiLavoro(Tipologia));
+
+CREATE TABLE CicloDiLavoro(
+    Tipologia VARCHAR(20) PRIMARY KEY);
+
 CREATE TABLE Registrazione(
-Articolo ,
-Fornitore ,
-Documento,
-Quantità);
+    Articolo VARCHAR(8) NOT NULL REFERENCES Articolo(Codice),
+    Fornitore INTEGER NOT NULL REFERENCES Fornitore(Codice),
+    Documento INTEGER NOT NULL REFERENCES Documento(Numero),
+    Quantità INTEGER NOT NULL);
+
 CREATE TABLE Fabbricazione(
-Articolo,
-Quantità,
-Commessa);
+    Articolo VARCHAR(8) NOT NULL REFERENCES Articolo(codice),
+    Commessa INTEGER NOT NULL REFERENCES MacchinaInLavorazione(Matricola));
+
 CREATE TABLE Commessa(
-CodiceCommessa integer,
-DataInizio date,
-DataFine DATE,
-CodiceArticolo VARCHAR(8),
-Matricola INTEGER);
-CREATE TABLE Impegno(Ordine, Commessa)
+    CodiceCommessa integer NOT NULL,
+    DataInizio date NOT NULL,
+    DataFine DATE NOT NULL ,
+    CodiceArticolo VARCHAR(8) NOT NULL,
+    Matricola INTEGER PRIMARY KEY);
+
+CREATE TABLE Impegno(
+    Ordine INTEGER REFERENCES Ordine(Numero),
+    Commessa INTEGER REFERENCES Commessa(CodiceCommessa));
+
 CREATE TABLE Ordine(
-Numero INTEGER,
-Data DATE,
-Importo INTEGER,
-Codice INTEGER,
-Quantità INTEGER);
+    Numero INTEGER PRIMARY KEY AUTO_INCREMENT,
+    DataOrdine  DATE NOT NULL,
+    Importo INTEGER NOT  NULL,
+    Codice VARCHAR(8) NOT NULL,
+    Quantità INTEGER NOT NULL);
 
 CREATE TABLE Richiesta(
     Cliente INTEGER NOT NULL REFERENCES Cliente(Codice),
@@ -100,10 +108,10 @@ CREATE TABLE Documento(
     TipoDocumento VARCHAR(10) NOT NULL);
 
 CREATE TABLE MacchinaInLavorazione(
-    Matricola INTEGER PRIMARY KEY ,
+    Matricola INTEGER PRIMARY KEY,
     Codice VARCHAR(8) NOT NULL);
 
 CREATE TABLE Composizione(
-    Matricola INTEGER PRIMARY KEY,
+    Matricola INTEGER REFERENCES MacchinaInLavorazione(Matricola),
     Codice VARCHAR(8) NOT NULL,
     Quantità INTEGER NOT NULL);
