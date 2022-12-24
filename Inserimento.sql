40e4392df2233616e1206b2b812f851887bf5fde
ivanpacenti <s1103326@studenti.univpm.it>
2022-12-24 17:33:28 +0100

    introdotto articoli

---
+++ Inserimento.sql
@@ -0,0 +1,13 @@
+USE Messersi;
+INSERT INTO Articolo (Codice, Descrizione, Quantità, Prezzo, Altezza, Larghezza, Profondità, Massa)
+VALUES ('55141618','TELAIO SOTTOCARRO CH2R','10','1200','3','1','6','120');
+INSERT INTO Articolo (Codice, Descrizione, Quantità, Prezzo, Altezza, Larghezza, Profondità, Massa)
+VALUES ('55030790','TELAIO POSTO GUIDA','15','350','1.5','1.5','1','20');
+INSERT INTO Articolo (Codice, Descrizione, Quantità, Prezzo, Altezza, Larghezza, Profondità, Massa)
+VALUES ('55030450','TELAIO POSTO GUIDA CH2R','10','1200','3','1','6','120');
+INSERT INTO Articolo (Codice, Descrizione, Quantità, Prezzo, Altezza, Larghezza, Profondità, Massa)
+VALUES ('55181768','TELAIO SOTTOCARRO CH2R','10','1200','3','1','6','120');
+INSERT INTO Articolo (Codice, Descrizione, Quantità, Prezzo, Altezza, Larghezza, Profondità, Massa)
+VALUES ('09681212','SOTTOCARRO CINGOLATO CH2N13','10','1200','3','1','6','120');
+INSERT INTO Articolo (Codice, Descrizione, Quantità, Prezzo, Altezza, Larghezza, Profondità, Massa)
+VALUES ('45157898','BENNA TC550D','10','1200','3','1','6','120');
