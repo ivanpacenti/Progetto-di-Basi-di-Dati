2b2059f4d93ca3a3390c683c4d09765791b11f07
nicopiccia <nicola.picciafuoco@gmail.com>
2022-12-27 22:44:17 +0100

    ELIMAZIONE FILE PER LAVORARE E inserimento macchine in lavorazione

--- Inserimento.sql
+++ Inserimento.sql
@@ -128,6 +128,56 @@ INSERT INTO Locazione(PesoOccupato, Sezione, Scaffalatura, RigaScaff, ColonnaSca
 VALUES ('0','A','a','4','5') ;
 
 
+/* INSERIMENTO CLIENTI */
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('Ivan S.P.A.','02011234321');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('BARTOLINI S.A.S','09812343212');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('PICCIA','03947264857');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('VINO BUONO','93847361530');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('MACCHINE INDUSTRIALI','04938572613');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('LUCA & STEFANO','12345678900');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('GIACOMINO','09876543215');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('CASA DELLE MACCHINE','56374819043');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('CASA DELLA RUSPA','37584930193');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('IL RUSPAIOLO','46372894817');
+
+/* INSERIMENTO FORNITORI */
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('TUTTO VITI','75248262774');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('TAGLIO LASER S.A.S','76923450854');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('LAMIERE DOC','76888340320');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('DI BRUNO E STEFANO','50496876298');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('GIOCO E LUCE','12675565641');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('LAMPADINI','42719228824');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('GIANLUCHINI','72427555639');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('OGNI TUBO','71480986277');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('TUTTO TUBI','20452911970');
+INSERT INTO cliente(RagioneSociale, PartitaIVA)
+        VALUE ('FILI.IT','86285140257');
+
+
+/* INSERIMENTO MACCHINA IN LAVORAZIONE*/
+INSERT INTO macchinainlavorazione(Codice) VALUES ('09681212');
+INSERT INTO macchinainlavorazione(Codice) VALUES ('09576488');
+INSERT INTO macchinainlavorazione(Codice) VALUES ('09680200');
+INSERT INTO macchinainlavorazione(Codice) VALUES ('09508080');
 
 /*INSERIMENTO UBICAZIONI*/
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
