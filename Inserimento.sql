ff5fe0ad509b822c3e002f71d799c6ed4883c1f7
ivanpacenti <s1103326@studenti.univpm.it>
2022-12-27 16:32:04 +0100

    ordinati comandi creazioni tabelle

--- Inserimento.sql
+++ Inserimento.sql
@@ -8,9 +8,22 @@ VALUES ('55030450','TELAIO POSTO GUIDA CH2R','10','1200','3','1','6','120');
 INSERT INTO Articolo (Codice, Descrizione, Quantità, Prezzo, Altezza, Larghezza, Profondità, Massa)
 VALUES ('55181768','TELAIO SOTTOCARRO CH2R','10','1200','3','1','6','120');
 INSERT INTO Articolo (Codice, Descrizione, Quantità, Prezzo, Altezza, Larghezza, Profondità, Massa)
-VALUES ('09681212','SOTTOCARRO CINGOLATO CH2N13','10','1200','3','1','6','120');
+VALUES ('45157898','BENNA TC550D','10','1200','3','1','6','120');
 INSERT INTO Articolo (Codice, Descrizione, Quantità, Prezzo, Altezza, Larghezza, Profondità, Massa)
 VALUES ('45157898','BENNA TC550D','10','1200','3','1','6','120');
+
+INSERT INTO Articolo (Codice, Descrizione, Quantità, Prezzo, Altezza, Larghezza, Profondità, Massa)
+VALUES ('09681212','MINI-DUMPER TC85d','10','13000','0.5','0.6','1','850');
+INSERT INTO Articolo (Codice, Descrizione, Quantità, Prezzo, Altezza, Larghezza, Profondità, Massa)
+VALUES ('09576488','MINI-DUMPER TC50-e','10','15000','0.5','0.6','1','500');
+INSERT INTO Articolo (Codice, Descrizione, Quantità, Prezzo, Altezza, Larghezza, Profondità, Massa)
+VALUES ('09680200','MINI-ESCAVATORE M38U','10','18000','1.3','1.3','0.8','3600');
+INSERT INTO Articolo (Codice, Descrizione, Quantità, Prezzo, Altezza, Larghezza, Profondità, Massa)
+VALUES ('09782020','MINI-ESCAVATORE M18DS','10','17000','1.3','1.3','0.8','1850');
+INSERT INTO Articolo (Codice, Descrizione, Quantità, Prezzo, Altezza, Larghezza, Profondità, Massa)
+VALUES ('09203999','CTL 45','10','28000','1','1.3','1','2700');
+INSERT INTO Articolo (Codice, Descrizione, Quantità, Prezzo, Altezza, Larghezza, Profondità, Massa)
+VALUES ('09508080','ROBOKLIN 25','10','25000','2.5','2','3','4200');
 
 
 INSERT INTO Locazione( CodiceScaffale,PesoOccupato, Sezione, Scaffalatura, RigaScaff, ColonnaScaff)
@@ -39,11 +52,11 @@ INSERT INTO Locazione( CodiceScaffale,PesoOccupato, Sezione, Scaffalatura, RigaS
 VALUES ('12','0','A','a','3','3');
 INSERT INTO Locazione( CodiceScaffale,PesoOccupato, Sezione, Scaffalatura, RigaScaff, ColonnaScaff)
 VALUES ('13','0','A','a','3','4');
-INSERT INTO Locazione( CodiceScaffale,PesoOccupato, Sezione, Scaffalatura, RigaScaff, ColonnaScaff)
-VALUES ('14','0','A','a','3','5');
+INSERT INTO Locazione( PesoOccupato, Sezione, Scaffalatura, RigaScaff, ColonnaScaff)
+VALUES ('0','A','a','3','5');
 
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
 VALUES ('5','55141618','20');
 
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('2','09681212','20');
+VALUES ('2','45157898','20');
