70eec7f15944a21445844bb2ead92a4d7677ccdc
nicopiccia <nicola.picciafuoco@gmail.com>
2022-12-28 16:35:37 +0100

    INSERIMENTO POPOLAMENTO RICHIESTA

---
+++ APPOGGIO.sql
@@ -0,0 +1,20 @@
+/* POPOLAMENTO RICHIESTA */
+
+INSERT INTO richiesta(Cliente, Ordine)
+VALUE (1,1);
+
+INSERT INTO richiesta(Cliente, Ordine)
+VALUE (2,2);
+
+INSERT INTO richiesta(Cliente, Ordine)
+VALUE (3,3);
+
+INSERT INTO richiesta(Cliente, Ordine)
+VALUE (4,4);
+
+INSERT INTO richiesta(Cliente, Ordine)
+VALUE (4,5);
+
+/* POPOLAMENTO IMPEGNO */
+INSERT INTO richiesta(Cliente, Ordine)
+VALUE (4,5);
