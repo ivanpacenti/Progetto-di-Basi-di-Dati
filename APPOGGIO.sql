504730d061a3597d2bdc93d7068b1147f5d06be4
nicopiccia <nicola.picciafuoco@gmail.com>
2022-12-28 17:01:27 +0100

    DELETE IMPEGNO

--- APPOGGIO.sql
+++ APPOGGIO.sql
@@ -11,19 +11,3 @@ VALUE (3,3);
 
 INSERT INTO richiesta(Cliente, Ordine)
 VALUE (4,4);
-
-/* POPOLAMENTO IMPEGNO */
-INSERT INTO impegno(ordine, commessa)
-    VALUE(1,1);
-INSERT INTO impegno(ordine, commessa)
-    VALUE(2,2);
-INSERT INTO impegno(ordine, commessa)
-    VALUE(3,3);
-INSERT INTO impegno(ordine, commessa)
-    VALUE(4,4);
-INSERT INTO impegno(ordine, commessa)
-    VALUE(4,5);
-
-
-
-
