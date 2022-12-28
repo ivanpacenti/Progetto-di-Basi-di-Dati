4c2824fe05da5e74dadb8198b0d702975fc69c2f
nicopiccia <nicola.picciafuoco@gmail.com>
2022-12-28 15:54:13 +0100

    FIX APPOGGIO

--- Inserimento.sql
+++ Inserimento.sql
@@ -242,3 +242,40 @@ VALUES ('5','55141618','20');
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
 VALUES ('2','09681212','20');
 
+/* DOCUMENTO*/
+INSERT INTO documento(DatiPagamento, DataDocumento, Importo, TipoDocumento)
+    VALUE('IT32X0300203280863596584462','2022-07-25',13000,'Fattura');
+INSERT INTO documento(DatiPagamento,DataDocumento,Importo, TipoDocumento)
+    VALUE('0','2022-07-25',0,'DDT');
+
+INSERT INTO documento(DatiPagamento,DataDocumento,Importo, TipoDocumento)
+    VALUE('IT32X0300203280863596584462','2022-9-20',15000,'FATTURA');
+INSERT INTO documento(DatiPagamento,DataDocumento,Importo, TipoDocumento)
+    VALUE('0','2022-9-20',0,'DDT');
+
+INSERT INTO documento(DatiPagamento,DataDocumento,Importo, TipoDocumento)
+    VALUE('IT32X0300203280863596584462','2022-11-02',18000,'FATTURA');
+INSERT INTO documento(DatiPagamento,DataDocumento,Importo, TipoDocumento)
+    VALUE('0','2022-11-02',0,'DDT');
+
+INSERT INTO documento(DatiPagamento,DataDocumento,Importo, TipoDocumento)
+    VALUE('IT32X0300203280863596584462','2022-12-21',50000,'FATTURA');
+INSERT INTO documento(DatiPagamento,DataDocumento,Importo, TipoDocumento)
+    VALUE('0','2022-12-21',0,'DDT');
+
+
+/*INSERIMENTO COMMESSA */
+INSERT INTO commessa( datainizio, datafine)
+    VALUES ('2022-07-01','2022-07-20');
+
+INSERT INTO commessa( datainizio, datafine)
+    VALUES ('2022-08-29','2022-9-18');
+
+INSERT INTO commessa( datainizio, datafine)
+    VALUES ('2022-09-30','2022-10-01');
+
+INSERT INTO commessa( datainizio, datafine)
+    VALUES ('2022-10-22','2022-11-12');
+
+INSERT INTO commessa( datainizio, datafine)
+    VALUES ('2022-11-15','2022-11-30');
