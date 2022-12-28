3aad4eca1f98e2e77aa359c14f1b055c6c8fb444
nicopiccia <nicola.picciafuoco@gmail.com>
2022-12-28 18:29:06 +0100

    CLIENTI E FORNITORI VIE

---
+++ APPOGGIO.sql
@@ -0,0 +1,65 @@
+/*  INSERIMENTO SEDI CLIENTI */
+INSERT INTO sediclienti(Cliente, Strada, Civico, Comune)
+    VALUE (1,'via ascoli piceno',1,'Ancona');
+
+INSERT INTO sediclienti(Cliente, Strada, Civico, Comune)
+    VALUE (2,'via ruspe grosse',5,'Padova');
+
+INSERT INTO sediclienti(Cliente, Strada, Civico, Comune)
+    VALUE (3,'via dante alighieri',18,'Firenze');
+
+INSERT INTO sediclienti(Cliente, Strada, Civico, Comune)
+    VALUE (4,'via del vino',3,'OSTRA');
+
+INSERT INTO sediclienti(Cliente, Strada, Civico, Comune)
+    VALUE (5,'via Liguria',7,'Civitanova Marche');
+
+INSERT INTO sediclienti(Cliente, Strada, Civico, Comune)
+    VALUE (6,'via del giogo',1,'Ravenna');
+
+INSERT INTO sediclienti(Cliente, Strada, Civico, Comune)
+    VALUE (7,'via dei cagnoli',10,'Ancona');
+
+INSERT INTO sediclienti(Cliente, Strada, Civico, Comune)
+    VALUE (8,'via della storta',1,'Roma');
+
+INSERT INTO sediclienti(Cliente, Strada, Civico, Comune)
+    VALUE (9,'via marzocca',34,'Senigallia');
+
+INSERT INTO sediclienti(Cliente, Strada, Civico, Comune)
+    VALUE (10,'via milano ',19,'Milano');
+
+/* SEDI FORNITORI */
+
+INSERT INTO sedifornitori(fornitore, strada, civico, comune)
+    VALUE(1,'via del sole',10,'Mondolfo') ;
+
+INSERT INTO sedifornitori(fornitore, strada, civico, comune)
+    VALUE(2,'via della luna',15,'Falconara') ;
+
+INSERT INTO sedifornitori(fornitore, strada, civico, comune)
+    VALUE(3,'via delle stelle ',20,' Aosta') ;
+
+INSERT INTO sedifornitori(fornitore, strada, civico, comune)
+    VALUE(4,'via emilia',1,'Rimini') ;
+
+INSERT INTO sedifornitori(fornitore, strada, civico, comune)
+    VALUE(5,'Via senza indugi',10,'Ancona') ;
+
+INSERT INTO sedifornitori(fornitore, strada, civico, comune)
+    VALUE(6,'via mare grosso',17,'Macerata') ;
+
+INSERT INTO sedifornitori(fornitore, strada, civico, comune)
+    VALUE(7,'via toledo',107,'Napoli') ;
+
+INSERT INTO sedifornitori(fornitore, strada, civico, comune)
+    VALUE(8,'via Marco enorme',56,'Ascoli') ;
+
+INSERT INTO sedifornitori(fornitore, strada, civico, comune)
+    VALUE(9,'via senza fretta',98,'Bari') ;
+
+INSERT INTO sedifornitori(fornitore, strada, civico, comune)
+    VALUE(10,'via del sole',100,'Catania') ;
+
+INSERT INTO sedifornitori(fornitore, strada, civico, comune)
+    VALUE(11,'via Arceviese',56,'Marotta') ;
