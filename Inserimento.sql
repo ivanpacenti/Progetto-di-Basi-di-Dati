52f08978bce363c7957c8da013ef3453c51d2518
nick <nicolobartolinii@gmail.com>
2023-01-02 15:58:31 +0100

    correzione stringhe in Inserimento.sql 2

--- Inserimento.sql
+++ Inserimento.sql
@@ -285,53 +285,53 @@ INSERT INTO Ordine(DataOrdine, Importo, Codice, Quantità)
 
 /*INSERIMENTO UBICAZIONI*/
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('1','09508080',10);
+VALUES (1,'09508080',10);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('1','09203999',10);
+VALUES (1,'09203999',10);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('1','09782020','10');
+VALUES (1,'09782020',10);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('2','04543000','2000');
+VALUES (2,'04543000',2000);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('3','04543000','100');
+VALUES (3,'04543000',100);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('4','04543000','100');
+VALUES (4,'04543000',100);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('5','04369000','100');
+VALUES (5,'04369000',100);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('6','04369000','100');
+VALUES (6,'04369000',100);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('8','04369000','20');
+VALUES (8,'04369000',20);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('8','04126300','40');
+VALUES (8,'04126300',40);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('9','04126300','50');
+VALUES (9,'04126300',50);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('10','04126300','20');
+VALUES (10,'04126300',20);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('11','30152800','2');
+VALUES (11,'30152800',2);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('12','04126300','50');
+VALUES (12,'04126300',2);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('12','04427000','2');
+VALUES (12,'04427000',2);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('13','04427000','2');
+VALUES (13,'04427000',2);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('14','04427000','2');
+VALUES (14,'04427000',2);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('15','04427000','2');
+VALUES (15,'04427000',2);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('16','05990020','2');
+VALUES (16,'05990020',2);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('17','05990020','3');
+VALUES (17,'05990020',3);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('18','05990030','1');
+VALUES (18,'05990030',1);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('19','05990030','2');
+VALUES (19,'05990030',2);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('20','05990040','2');
+VALUES (20,'05990040',2);
 INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
-VALUES ('21','05990040','1');
+VALUES (21,'05990040',1);
 INSERT INTO ubicazione(Locazione, Articolo, Quantita)
 VALUE (1,'30156001',10);
 INSERT INTO  ubicazione(Locazione, Articolo, Quantita)
