5e2540b7819a3d9390ecff9132d3303d938dca67
ivanpacenti <s1103326@studenti.univpm.it>
2023-01-02 15:55:44 +0100

    modificati integer values

---
+++ appoggiodomenico.sql
@@ -0,0 +1,53 @@
+/*INSERIMENTO UBICAZIONI*/
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (1,'09508080',10);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (1,'09203999',10);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (1,'09782020',10);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (2,'04543000',2000);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (3,'04543000',100);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (4,'04543000',100);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (5,'04369000',100);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (6,'04369000',100);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (8,'04369000',20);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (8,'04126300',40);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (9,'04126300',50);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (10,'04126300',20);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (11,'30152800',2);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (12,'04126300',2);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (12,'04427000',2);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (13,'04427000',2);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (14,'04427000',2);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (15,'04427000',2);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (16,'05990020',2);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (17,'05990020',3);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (18,'05990030',1);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (19,'05990030',2);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (20,'05990040',2);
+INSERT INTO Ubicazione( Locazione,Articolo, Quantita)
+VALUES (21,'05990040',1);
+INSERT INTO ubicazione(Locazione, Articolo, Quantita)
+VALUE (1,'30156001',10);
+INSERT INTO  ubicazione(Locazione, Articolo, Quantita)
+VALUE(1,'45150898',10);
